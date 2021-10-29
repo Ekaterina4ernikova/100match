6dea3f2c832d395e3ccfd36164a33fe81b940d9f
Ekaterina4ernikova <chereshnya159@icloud.com>
2021-10-30 00:59:32 +0300

    ver10

--- test/test.cpp
+++ test/test.cpp
@@ -2,19 +2,19 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch/catch.hpp"
 
-TEST_CASE("Test Amount")
+TEST_CASE("Test Allowable Amount")
 {
     bool expected = true;
     REQUIRE(check_number_match(2, 100) == expected);
 }
 
-TEST_CASE("Test Difference")
+TEST_CASE("Test Max Allowable Amount")
 {
     bool expected = true;
     REQUIRE(check_number_match(10, 100) == expected);
 }
 
-TEST_CASE("Test Multiplication")
+TEST_CASE("Test Invalid Amount")
 {
     bool expected = false;
     REQUIRE(check_number_match(15, 100) == expected);
