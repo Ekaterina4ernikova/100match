5c8402f19f1add1d714dc5abd7ad5b90e93b3a76
Ekaterina4ernikova <92290855+Ekaterina4ernikova@users.noreply.github.com>
2021-10-29 19:22:44 +0300

    Delete test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include "100 спичек.h"
-
-void unit_test()
-{
-    assert(check_number_match(2, 100) == true);
-    assert(check_number_match(10, 100) == true);
-    assert(check_number_match(15, 100) == false);
-    std::cout << "Unit-tests выполнены!\n\n";
-}
