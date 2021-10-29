e21a6431a1a1b580e902d216226548e68877c1e7
Ekaterina4ernikova <chereshnya159@icloud.com>
2021-10-30 00:21:11 +0300

    rebuild

--- 100math.cpp
+++ 100math.cpp
@@ -1,65 +1,10 @@
 ﻿
-#include "100math.h"
+#include "function.h"
 
 
 using namespace std;
 
 const int InitialCount = 100;
-
-
-
-
-void game()
-{
-  int Count, Num, Player;
-  bool Correct;
-
-  Player = 2;
-  Count = InitialCount;
-
-    do {
-         if (Player == 1)
-                Player = 2;
-         else
-                Player = 1;
-        if (Player == 1) {
-            do {
-                std::cout << "Ваш ход. На столе " << Count << " спичек.\n";
-                std::cout << "Сколько спичек Вы берете?\n";
-                std::cin >> Num;
-                Correct = check_number_match(Num, Count);
-                } while (!Correct);
-            }
-            else
-            {
-                do {
-                    Num = rand() % 10 + 1;
-                    if (Num > Count)
-                        Num = Count;
-                    std::cout << "Мой ход. Я взял " << Num << " спичек\n";
-                } while (!Correct);
-            }
-            Count -= Num;
-
-        } while (Count > 0);
-
-        if (Player == 1)
-            std::cout << "Вы победили!\n";
-        else
-            std::cout << "Вы проиграли!\n";
-    }
-
-void print_menu() {
-    cout << "Выберете пункт меню: " << endl;
-    cout << "1. Правила" << endl;
-    cout << "2. Играть" << endl;
-    cout << "3. Выход" << endl;
-    cout << ">";
-}
-
-void prav() {
-    cout << " Из кучки, первоначально содержащей 100 спичек, двое играющих поочередно\n берут по несколько спичек: не менее одной и не более десяти.\n Выигрывает взявший последнюю спичку." << endl;
-}
 
 int main() {
     setlocale(LC_ALL, "Russian");
--- function.h
+++ function.h
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cassert>
 
+using namespace std;
 
 bool check_number_match(int n_pl_match, int quant);
 void game();
